428d493b239a8a2cf913fc971c90fbb999df3281
Sergey Kudashev <kudashevs@gmail.com>
2025-03-13 16:27:02 +0300

    refactor: prepare for the sixth step

--- src/greeter.ts
+++ src/greeter.ts
@@ -3,15 +3,15 @@ type Greetable = string[] | string | null;
 export default class Greeter {
   greet(who: Greetable): string {
     if (Array.isArray(who)) {
-      return `Hello, ${this.concatenate(who)}.`;
+      return this.normalGreeting(this.concatenate(who));
     }
 
     if (who === null) {
-      return "Hello, my friend.";
+      return this.normalGreeting('my friend');
     }
 
     if (this.isShouting(who)) {
-      return `HELLO, ${who}!`;
+      return this.shoutGreeting(who);
     }
 
     return `Hello, ${who}.`;
@@ -21,7 +21,17 @@ export default class Greeter {
     return who === who.toUpperCase();
   }
 
-  private concatenate(who: string[]) : string{
+  private normalGreeting(who: string): string {
+    return `Hello, ${who}.`;
+  }
+
+  private shoutGreeting(who: string): string {
+    return this.normalGreeting(who)
+      .replace(/\.$/, '!')
+      .toUpperCase();
+  }
+
+  private concatenate(who: string[]): string {
     return who.length === 2
       ? who.join(' and ')
       : who.slice(0, -1).join(', ') + ', and ' + who.slice(-1);
